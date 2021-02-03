019c9927d9e5a0b515a3300015022484759cb7b1
ottoflytstrom <56597291+ottoflytstrom@users.noreply.github.com>
2021-02-03 09:21:22 +0100

    Creating project

---
+++ TentapluggVT/TentapluggVT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TentapluggVT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
