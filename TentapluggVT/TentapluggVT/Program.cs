984d98792d88985ff04ab95f4dee9b9c1560c496
Valdemar99 <va7012an-s@student.lu.se>
2021-02-03 09:36:50 +0100

    Created delegates

--- TentapluggVT/TentapluggVT/Program.cs
+++ TentapluggVT/TentapluggVT/Program.cs
@@ -4,9 +4,23 @@ namespace TentapluggVT
 {
     class Program
     {
+        public delegate void MyDelegate(int i);
+
+        public static void Call(MyDelegate myDelegate1, MyDelegate myDelegate2)
+        {
+            myDelegate1(1);
+            myDelegate2(30);
+        }
+        static void F1(int i) { Console.WriteLine("callback metodf1 " + i); }
+        static void F2(int i) { Console.WriteLine("callback metodf2 " + i); }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyDelegate m1 = new MyDelegate(F1);
+            m1(30);
+            F1(30);
+            Console.ReadLine();
+            //MyDelegate m2
         }
     }
 }
